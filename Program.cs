fd8a47a9caf65064963c5410b11edf1c15f83232
Rror1K <116387515+Rror1K@users.noreply.github.com>
2024-01-23 16:56:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Word_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text");
+            Console.Write(">>> "); string usText = Console.ReadLine();
+
+            int usLeng = usText.Length;
+
+            Console.WriteLine($"You have entered {usLeng} characters");
+        }
+    }
+}
